f770896a0a660891a18884150307b3be1e13a3f6
Camilo Gutierrez <cagutierrezra@unal.edu.co>
2020-11-27 23:47:54 -0500

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 FONT_AWESOME = "https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP,FONT_AWESOME], title='Equipo Rayo', update_title=None)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP,FONT_AWESOME], title='SIMTE App', update_title=None)
 server = app.server
 
 # We need this for function callbacks not present in the app.layout
